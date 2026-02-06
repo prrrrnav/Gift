a2243e31423c4c3a1ab778c7ea0255635747b2e0
Pranav <Pranav143010@gmail.com>
2026-02-06 21:26:26 +0530

    Move dist to docs for GitHub Pages

--- docs/js/index.js
+++ docs/js/index.js
@@ -55,7 +55,7 @@
     }), 2 * commentTime);
   };
 
-  $('body').append("  <style id=\"style-tag\"></style>\n<span id=\"echo\"></span>\n<span id=\"heart\"><i></i></span>\n<pre id=\"style-text\"></pre>\n  <div style = 'position: fixed;left: 30px;bottom: 10px;'>\n    <a id='skipAnimation'>跳过动画</a>\n    <a style='color: #fff' href='http://love.idealclover.top'>重新播放</a>\n    <a style='color: #fff' target=\"_blank\" href='https://github.com/idealclover/Love-Gift'>项目地址</a>\n    <a style='color: #fff' target=\"_blank\" href='https://www.idealclover.top/'>我的博客</a>\n  </div>");
+  $('body').append("  <style id=\"style-tag\"></style>\n<span id=\"echo\"></span>\n<span id=\"heart\"><i></i></span>\n<pre id=\"style-text\"></pre>\n  <div style = 'position: fixed;left: 30px;bottom: 10px;'>\n    <a id='skipAnimation'></a>\n    <a style='color: #fff' href='http://love.idealclover.top'></a>\n    <a style='color: #fff' target=\"_blank\" href='https://github.com/idealclover/Love-Gift'></a>\n    <a style='color: #fff' target=\"_blank\" href='https://www.idealclover.top/'></a>\n  </div>");
 
   commentTime = 60;
 
